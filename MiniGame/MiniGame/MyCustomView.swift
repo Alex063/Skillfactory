c127a3c17afa1b7954d1ac1ea24fc1d014e48fbc
Alex063 <aleksandr.gulakov@gmail.com>
2023-01-07 17:42:30 +0500

    mini game
    mini game

---
+++ MiniGame/MiniGame/MyCustomView.swift
@@ -0,0 +1,42 @@
+//
+//  MyCustomView.swift
+//  MiniGame
+//
+//  Created by Aleksandr Gulakov on 06.01.2023.
+//
+
+import UIKit
+
+class MyCustomView: UIView {
+    
+    var workingView: UIView!
+    var xibName: String = "MyCustomView"
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setCustomView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setCustomView()
+    }
+    
+    func getFromXib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let xib = UINib(nibName: xibName, bundle: bundle)
+        let view = xib.instantiate(withOwner: self, options: nil).first as! UIView
+        
+        return view
+    }
+    
+    func setCustomView() {
+        workingView = getFromXib()
+        workingView.frame = bounds
+        workingView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        workingView.layer.cornerRadius = frame.size.width / 2
+        addSubview(workingView)
+    }
+    
+     
+}
